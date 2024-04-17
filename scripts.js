b57efd721aba6de68e49ef53714bda6901a3780e
robin.navratil <robin.navratil@office365.spspzlin.cz>
2024-04-17 11:03:04 +0200

    durable

--- scripts.js
+++ scripts.js
@@ -1,30 +1,60 @@
-let btn = document.querySelector('#new-quote');
-let quote = document.querySelector('.quote');
-let person = document.querySelector('.person');
-
-const quotes = [{
-    quote: `"Základní car detailing"`,
-    person: `Čištění a voskování exteriéru vozidla, včetně umytí, ošetření povrchu a leštění. `
-}, {
-    quote: `"Detailing interiéru"`,
-    person: `  Čištění a oživení interiéru vozidla, včetně čalounění, plastů, skel a všech dalších povrchů.`
-}, {
-    quote: `"Nanolakování a ochrana povrchu: "`,
-    person: `Aplikace speciálních ochranných prostředků, jako je nanolakování, keramické povlaky nebo vosky, které chrání lak vozidla před poškozením a znečištěním.`
-}, {
-    quote: `"Odstranění vodních skvrn a drobných škrábanců"`,
-    person: ` Použití specializovaných technik a produktů k odstranění vodních skvrn, lepidel, žvýkačky, drobných škrábanců a dalších defektů na povrchu vozidla.`
-}, {
-    quote: `"Čištění motoru"`,
-    person: `Důkladné čištění motorového prostoru od prachu, oleje a jiných nečistot..`
-
-}, ]
-
-btn.addEventListener('click', function(){
-
-    let random = Math.floor(Math.random() * quotes.length);
-
-    quote.innerText = quotes[random].quote;
-    person.innerText = quotes[random].person;
-
-})
+const carousel = document.querySelector(".carousel"),
+    firstImg = carousel.querySelectorAll("img")[0],
+    arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+});
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
